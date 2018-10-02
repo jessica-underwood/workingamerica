686354e6f6afa812e5459b8942ef2f83ae516397
Jessica Underwood <jessica.lynne.underwood@gmail.com>
2018-10-01 21:26:49 -0400

    combining data

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,29 @@
-// var link = require("../../gz_2010_us_040_00_500k.json");
+
+
+
 
 // // Creating map object
 // var map = L.map("map", {
 //   center: [40.7128, -74.0059],
 //   zoom: 3
 // });
+// console.log(link);
+console.log(data[0].State);
+// iterate over link, for each match state name, push/append object.keys 
+// properties.data 
+
+for (let i = 0; i < link.features.length; i++) {
+  // console.log(link.features[i].properties.NAME);
+  for (let j = 0; j < data.length; j++) {
+    // console.log("data" + data[j].State);
+    if (link.features[i].properties.NAME == data[j].State) {
+      console.log("Bingo!");
+      link.features[i].properties.data = data[j];
+    }
+  }
+}
+
+console.log(link);
 
 // // Adding tile layer
 // L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -15,8 +34,8 @@
 // }).addTo(map);
 // Creating map object
 var map = L.map("map", {
-  center: [40.7128, -74.0059],
-  zoom: 5
+  center: [39.8283, -98.5795],
+  zoom: 3
 });
 
 // Adding tile layer
@@ -28,11 +47,22 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
  
 // var link ="../../gz_2010_us_040_00_500k.json";
-
+var mapStyle = {
+  color: "white",
+  fillColor: "pink",
+  fillOpacity: 0.5,
+  weight: 1.5
+};
 
 // var link = "../../gz_2010_us_040_00_500k.json"
 // Grabbing our GeoJSON data..
 //d3.json(link, function(data) {
   // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(link).addTo(map);
+  L.geoJson(link, {
+    style: mapStyle
+  }).addTo(map);
+
+  d3.json(function(data){
+
+  })
 // });
